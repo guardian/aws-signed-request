0c41c3ecedef77dd8032f4c728df70676485d29e
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-04-07 15:08:52 +0100

    send signed requests

---
+++ README.md
@@ -0,0 +1,75 @@
+# Signed AWS requests
+
+Module to send signed requests to an AWS service.
+
+Heavily inspired by [aws samples](https://github.com/awslabs/amazon-elasticsearch-lambda-samples).
+
+## Install
+
+```
+npm install --save aws-signed-request
+```
+
+## Usage
+
+### ElasticSearch
+
+```js
+var elastic = require('aws-signed-request')({
+	endpoint: 'htps://your.elasticsearch.es.amazon.com',
+	region: 'eu-west-1',
+	service: 'es'
+});
+
+elastic.send({
+	method: 'GET',
+	path: '/domain/index/id'
+}, function (err, data) {
+	console.log(data);
+});
+```
+
+### API Gateway
+
+```js
+var gateway = require('aws-signed-request')({
+	endpoint: 'https://your.api.gateway.amazon.com',
+	region: 'eu-west-1',
+	service: 'execute-api'
+});
+
+gateway.send({
+	method: 'POST',
+	path: '/action'
+}, function (err, data) {
+	console.log(data);
+});
+```
+
+
+### Advanced usage
+
+By default the module expects a JSON response. If you're expecting plain text you can call
+
+```js
+elastic.send({
+	method: 'GET',
+	path: '/_cat/indices',
+	json: false
+}, function (err, data) {
+	console.log(data); // as plain text
+});
+```
+
+If `json:true` and the response is not a valid JSON, the callback receives an error containing `responseText` for debug purposes.
+
+## Contribute
+
+Clone the repo, write some test, make them pass and pull request your changes.
+
+You can watch your tests by running
+
+```js
+npm install -g watch
+watch "npm test" . -d
+```
---
+++ index.js
@@ -0,0 +1,119 @@
+'use strict';
+
+/**
+ * Create the utility that sends signed request
+ *
+ * @param {aws-sdk} AWS sdk
+ * @param {Object} config Configuration containing
+ *    * endpoint: AWS endpoint
+ *    * region: AWS region
+ *    * service: AWS service
+ * @return {Object} Utility with method `send`
+ */
+module.exports = function (AWS, config) {
+	var awsEndpoint = new AWS.Endpoint(config.endpoint);
+
+	function sendRequest (params, callback) {
+		var req = new AWS.HttpRequest(awsEndpoint);
+		req.method = params.method.toUpperCase();
+		req.path = params.path;
+		req.region = config.region;
+		req.headers['presigned-expires'] = false;
+		req.headers['Host'] = awsEndpoint.host;
+		if (params.message) {
+			req.body = typeof params.message == 'string' ? params.message : JSON.stringify(params.message);
+		}
+
+		var creds = new AWS.EnvironmentCredentials('AWS');
+
+		var signer = new AWS.Signers.V4(req , config.service);
+		signer.addAuthorization(creds, new Date());
+
+		var send = new AWS.NodeHttpClient();
+		send.handleRequest(req, null, function (httpResp) {
+			handleSuccess(httpResp, params.json === false, callback);
+		}, function (err) {
+			handleFail(err, callback);
+		});
+	}
+
+	function handleSuccess (httpResp, asPlainText, callback) {
+		var respBody = '';
+		httpResp.on('data', function (chunk) {
+			respBody += chunk;
+		});
+		httpResp.on('end', function () {
+			if (httpResp.statusCode >= 200 && httpResp.statusCode < 400) {
+				if (asPlainText) {
+					process.nextTick(function () {
+						callback(null, respBody);
+					});
+				} else {
+					try {
+						var response = JSON.parse(respBody);
+						process.nextTick(function () {
+							callback(null, response);
+						});
+					} catch (ex) {
+						var error = new Error('Invalid JSON response');
+						error.responseText = respBody;
+						process.nextTick(function () {
+							callback(error);
+						});
+					}
+				}
+			} else {
+				var errorMessage;
+				try {
+					var errorResponse = JSON.parse(respBody);
+					errorMessage = errorResponse.Message;
+				} catch (ex) {
+					errorMessage = respBody;
+				}
+				process.nextTick(function () {
+					var error = new Error(errorMessage);
+					error.responseText = respBody;
+					callback(error);
+				});
+			}
+		});
+	}
+
+	function handleFail (err, callback) {
+		var error = typeof err === 'string' ? new Error(err) : err;
+		process.nextTick(function () {
+			callback(error);
+		});
+	}
+
+	return {
+		/**
+		 * Send a request
+		 * @param {Object} params Object containing
+		 *    * method: HTTP method (e.g GET, POST)
+		 *    * path: Request path (e.g. '/_cat/indices')
+		 *    * message: Request body (String or JSON)
+		 *    * json: Boolean, whether the response from the server should be parsed as JSON
+		 * @return {[type]} [description]
+		 */
+		send: function (params, callback) {
+			if (!params) {
+				process.nextTick(function () {
+					callback(new TypeError('Missing or invalid parameters'));
+				});
+			} else if (!params.method) {
+				process.nextTick(function () {
+					callback(new TypeError('Missing method'));
+				});
+			} else {
+				try {
+					sendRequest(params, callback);
+				} catch (ex) {
+					process.nextTick(function () {
+						callback(ex);
+					});
+				}
+			}
+		}
+	};
+};
